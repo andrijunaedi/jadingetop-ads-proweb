d394b0e8c328d9fb58abb606bb0400c4ec7b7155
Andri Junaedi <andri@sawala.tech>
2022-12-04 22:52:39 +0700

    feat: add current saldo

--- components/NavBar/index.php
+++ components/NavBar/index.php
@@ -1,6 +1,25 @@
 <header class="sticky top-0 z-10 py-5 pl-5 bg-white shadow mb-[0.10rem] pr-14">
     <nav class="flex justify-end">
         <div class="flex items-start space-x-5">
+            <div class="self-center font-semibold text-green-600">
+                <div class="flex items-center space-x-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a2.25 2.25 0 00-2.25-2.25H15a3 3 0 11-6 0H5.25A2.25 2.25 0 003 12m18 0v6a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 9m18 0V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v3" />
+                    </svg>
+                    <span>Rp. 100.000.000</span>
+                </div>
+            </div>
+
+            <div class="flex-shrink-0">
+                <button type="button" class="relative inline-flex items-center rounded-md border border-transparent bg-[#2B7FFF] px-4 py-2 text-sm font-medium text-white shadow-sm hover:hover:bg-[#6BA6FF] focus:outline-none focus:ring-2 focus:hover:bg-[#6BA6FF] focus:ring-offset-2">
+                    <!-- Heroicon name: mini/plus -->
+                    <svg class="-ml-1 mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
+                    </svg>
+                    <span>Tambah Saldo</span>
+                </button>
+            </div>
+
             <a>
                 <div class="relative mt-1 cursor-pointer">
                     <span class="absolute right-0 flex items-center justify-center w-4 h-4 text-xs font-semibold text-white bg-red-500 rounded-full -top-1">
