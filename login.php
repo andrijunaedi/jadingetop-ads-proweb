256cc37dfe31b4fc53ee15f944f1e523fac24cab
febiani16 <febiani481@gmail.com>
2022-12-09 18:58:59 +0700

    update login

--- login.php
+++ login.php
@@ -5,24 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login | Jadingetop Ads</title>
-    <link rel="stylesheet" href="stylelogin.css">
+    <link rel="stylesheet" href="./assets/styles/stylelogin.css">
 </head>
 <body>
     <div class="colspan">
         <h1 class="login">Masuk</h1>
         <form>
-            <label>Email</label><br>
-            <input class="text-input" type="text"><br><br>
-            <label>Kata sandi</label><br>
-            <input class="text-input" type="text"><br><br>
-            <input class="checkbox-input" type="checkbox">
-            <label>Ingat saya</label>
-            <a id="lupa_pw">Lupa kata sandi?</a><br><br>
-            <button id="Login">Masuk</button>
-            <a href="Masuk.html"></a>
+            <label for="email">Email</label><br>
+            <input id="email" class="text-input" type="text"><br><br>
+            <label for="kata-sandi">Kata sandi</label><br>
+            <input id="kata-sandi" class="text-input" type="text"><br><br>
+            <label for="ingt-saya">
+            <input id="ingt-saya" type="checkbox" name="ingat-saya" />Ingat saya</label>
+            <a href="#" class="lupa-password">Lupa kata sandi?</a><br><br>
+            <button class="masuk" type="button">Masuk</button>
         </form>
     </div>
 </body>
 
-<img src="Decorative image.png" class="gambar">
+<img src="assets/images/poster.png" class="gambar">
 </html>
