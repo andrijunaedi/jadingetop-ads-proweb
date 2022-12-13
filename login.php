fc951bd5721506e6dbee1fd5431b7a800bed5d86
febiani16 <115333668+febiani16@users.noreply.github.com>
2022-12-13 09:40:40 +0700

    feat: login page (#3)
    * login
    
    * ubh title
    
    * ubah title
    
    * update login
    
    * updt
    
    * ubh margin
    
    * Menambahkan name pd text-input & gambar di body
    
    * typo ingat saya

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login | Jadingetop Ads</title>
+    <link rel="stylesheet" href="./assets/styles/stylelogin.css">
+</head>
+<body>
+    <div class="colspan">
+        <h1 class="login">Masuk</h1>
+        <form>
+            <label for="email">Email</label><br>
+            <input id="email" name="email" class="text-input" type="text"><br><br>
+            <label for="kata-sandi">Kata sandi</label><br>
+            <input id="kata-sandi" name="password" class="text-input" type="text"><br><br>
+            <label for="ingat-saya">
+            <input id="ingat-saya" type="checkbox" name="ingat-saya" />Ingat saya</label>
+            <a href="#" class="lupa-password">Lupa kata sandi?</a><br><br>
+            <button class="masuk" type="button">Masuk</button>
+        </form>
+    </div>
+    <img src="assets/images/poster.png" class="gambar">
+</body>
+</html>
